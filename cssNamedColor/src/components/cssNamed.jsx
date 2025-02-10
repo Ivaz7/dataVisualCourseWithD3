2f9b1f7b5c7799508e2b02f74eb3921e60633aa8
Ivaz <ivazreza@gmail.com>
2025-02-10 16:00:30 +0700

    refractoring cssNamedData and making it to data states to use globally

--- cssNamedColor/src/components/cssNamed.jsx
+++ cssNamedColor/src/components/cssNamed.jsx
@@ -1,18 +1,35 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { useGetCSSColorsQuery } from '../service/redux/API/APIcsvCSSClr';
 import * as d3 from 'd3';
+import { setData } from '../service/redux/slice/dataSlice';
+import { useEffect } from 'react';
 
 const CssNamedColor = () => {
-  const { data } = useGetCSSColorsQuery();
+  const { data, isError, isLoading, error, reset } = useGetCSSColorsQuery();
+  const dataStored = useSelector((state) => state.dataSlice.dataStored);
+  const dispatch = useDispatch();
 
-  if (data) {
-    const parsedData = d3.csvParse(data);
+  useEffect(() => {
+    if (data) {
+      const parsedData = d3.csvParse(data);
+      dispatch(setData(parsedData));
+    }
+  }, [dispatch, data])
 
-    console.log(parsedData)
+  if (isError) {
+    alert(error)
+    reset();
   }
+
+  if (isLoading) {
+    return <div>Loading ...</div>
+  }
+
+  console.log(dataStored)
 
   return (
     <>
-    
+
     </>
   )
 }
--- cssNamedColor/src/service/redux/store.js
+++ cssNamedColor/src/service/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { cssColorAPIslice } from './API/APIcsvCSSClr';
+import dataReducer from './slice/dataSlice';
 
 export const store = configureStore({
   reducer: {
     [cssColorAPIslice.reducerPath]: cssColorAPIslice.reducer,
+    dataSlice: dataReducer,
   },
   middleware: (getDefaultMiddleware) => 
     getDefaultMiddleware().concat(cssColorAPIslice.middleware),
